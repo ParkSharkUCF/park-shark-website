4d6ef80b55c7a307bb07e703c9b274c0434cfe24
Keegan Conway <conwaykc@knights.ucf.edu>
2018-11-22 00:09:10 -0500

    initial commit

---
+++ src/Garage/Garage.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Garage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      garage: null,
+    };
+  }
+
+  async componentDidMount() {
+    const { match: { params } } = this.props;
+    const garage = (await axios.get(`https://murmuring-waters-47073.herokuapp.com/garage/${params.garageName}`)).data;
+
+    var occupied = {};
+
+    occupied[garage.garage[0].name] =  0;
+    for(var j = 0; j < garage.garage[0].sensors.length; j++) {
+      const sensor = (await axios.get(`https://murmuring-waters-47073.herokuapp.com/sensor/${garage.garage[0].sensors[j]}`)).data;
+      var count = sensor.sensor[0].cars;
+      occupied[garage.garage[0].name] += count;
+    }
+    garage.garage[0].occupied = occupied[garage.garage[0].name];
+
+    console.log(garage.garage[0].occupied)
+
+    this.setState({
+      garage,
+    });
+  }
+
+  render() {
+    const {garage} = this.state;
+    if (garage === null) return <p>Loading ...</p>;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="jumbotron col-12">
+            <h5 className="display-3">Parking Garage {garage.garage[0].name}</h5>
+            <p className="lead"></p>
+            <hr className="my-4" />
+            <p>Spots: {garage.garage[0].occupied}/{garage.garage[0].totalSpots}</p>
+            {
+              garage.occupied
+            }
+            <div id="myProgress">
+              <div id="myBar">{Math.floor((garage.garage[0].occupied/garage.garage[0].totalSpots) * 100)}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Garage;
---
+++ src/Garages/Garages.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+class Garages extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      garages: null,
+    };
+  }
+
+  async componentDidMount() {
+    var garages = (await axios.get('https://murmuring-waters-47073.herokuapp.com/garage')).data;
+    
+    var occupied = {};
+    for(var i = 0; i < garages.garages.length; i++) {
+      occupied[garages.garages[i].name] =  0;
+      for(var j = 0; j < garages.garages[i].sensors.length; j++) {
+        const sensor = (await axios.get(`https://murmuring-waters-47073.herokuapp.com/sensor/${garages.garages[i].sensors[j]}`)).data;
+        var count = sensor.sensor[0].cars;
+        occupied[garages.garages[i].name] = occupied[garages.garages[i].name] + count;
+      }
+      garages.garages[i].occupied = occupied[garages.garages[i].name];
+    }
+
+    this.setState({
+      garages
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          {this.state.garages === null && <p> Loading garages...</p>}
+          {
+            this.state.garages && this.state.garages.garages.map(garage => (
+              <div key={garage.name} className="col-sm-12 col-md-4 col-lg-3">
+                <Link to={`/garage/${garage.name}`}>
+                  <div className="card text-white bg-success mb-3">
+                    <div className="card-header">Parking Garage {garage.name}</div>
+                    <div className="card-body">
+                      <h4 className="card-title">Availability {garage.occupied}/{garage.totalSpots}</h4>
+                      <p className="card-text"></p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Garages;
