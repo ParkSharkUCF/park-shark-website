e12aad73218eacaa87158cdd5af21fe3127400cf
Keegan Conway <conwaykc@knights.ucf.edu>
2018-11-24 17:22:12 -0500

    added spot availability inofmration

--- src/Garage/Garage.js
+++ src/Garage/Garage.js
@@ -15,14 +15,18 @@ class Garage extends Component {
     const garage = (await axios.get(`https://murmuring-waters-47073.herokuapp.com/garage/${params.garageName}`)).data;
 
     var occupied = {};
+    garage.garage[0].spotAval = []
 
     occupied[garage.garage[0].name] =  0;
     for(var j = 0; j < garage.garage[0].sensors.length; j++) {
       const sensor = (await axios.get(`https://murmuring-waters-47073.herokuapp.com/sensor/${garage.garage[0].sensors[j]}`)).data;
       var count = sensor.sensor[0].cars;
+      garage.garage[0].spotAval.push(sensor.sensor[0].spots);
       occupied[garage.garage[0].name] += count;
     }
     garage.garage[0].occupied = occupied[garage.garage[0].name];
+
+    console.log(garage.garage[0].spotAval)
 
     this.setState({
       garage,
@@ -39,10 +43,18 @@ class Garage extends Component {
             <h5 className="display-3">Parking Garage {garage.garage[0].name}</h5>
             <p className="lead"></p>
             <hr className="my-4" />
-            <p>Spots: {garage.garage[0].occupied}/{garage.garage[0].totalSpots}</p>
+            <p>{garage.garage[0].occupied} remaining spots out of {garage.garage[0].totalSpots}</p>
+            <hr className="my-4" />
+            <div>
             {
-              garage.occupied
+              garage.garage[0].spotAval.map(spots => (
+                spots.map(spot => (
+                  <p>Spot {spot.spotID}: {spot.occupied} </p>
+                ))
+              ))
             }
+            </div>
+
             <div>
               <ProgressBar percentage={Math.floor((garage.garage[0].occupied/garage.garage[0].totalSpots) * 100)} />
             </div>
